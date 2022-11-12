e7bea7ce8fcba9cc8b02cab0f1302870341990fb
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2022-11-12 18:14:15 -0500

    Made minor adjustments to HTML/CSS, abstracted quiz end screen to its own function and started setup for dynamic quiz questions

--- script.js
+++ script.js
@@ -3,18 +3,36 @@ var restartButton = document.querySelector("#restart-button")
 var quizStart = document.querySelector("#quiz-start");
 var quizSection = document.querySelector("#quiz-questions");
 var quizEnd = document.querySelector("#quiz-end");
-console.log(quizSection.children[1].children[0].children);
 var questionList = quizSection.children[1].children[0];
+
+var questionFormat = {
+  question: "Which one is wrong?",
+  choices: ["Correct", "Correct", "Wrong", "Correct"],
+  correct: "Wrong"
+}
 
 function startQuiz() {
   quizStart.setAttribute("class", "hide");
   quizSection.setAttribute("class", "show");
+  setQuiz();
+}
+
+function endQuiz() {
+  quizSection.setAttribute("class", "hide");
+  quizEnd.setAttribute("class", "show");
+}
+
+function setQuiz() {
+  quizSection.getElementsByTagName("p").textContent = questionFormat.question;
+  for (let i = 0; i < questionList.children.length; i++) {
+    questionList.children[i].textContent = questionFormat.choices[i];
+  }
 }
 
 function clicked(evt) {
   console.log("This was clicked: " + evt.target.textContent);
-  quizSection.setAttribute("class", "hide");
-  quizEnd.setAttribute("class", "show");
+  showFeedback(evt.target.textContent)
+  endQuiz();
 }
 
 function restartQuiz(evt) {
