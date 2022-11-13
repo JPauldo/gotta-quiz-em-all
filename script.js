d790f0eba5a5b9f4c64b88928e717e5259c3a689
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2022-11-12 20:59:13 -0500

    Completed quiz loop and setup a question randomizer

--- script.js
+++ script.js
@@ -5,15 +5,47 @@ var quizSection = document.querySelector("#quiz-questions");
 var quizEnd = document.querySelector("#quiz-end");
 var questionList = quizSection.children[1].children[0];
 
-var questionFormat = {
-  question: "Which one is wrong?",
-  choices: ["Correct", "Correct", "Wrong", "Correct"],
-  correct: "Wrong"
+var questionsLeft;
+
+var quizQuestions;
+var qFormat;
+
+function getQuestions() {
+  quizQuestions = [
+    {
+      question: "Which one is wrong?",
+      choices: ["Correct", "Correct", "Wrong", "Correct"],
+      correct: "Wrong"
+    },
+    {
+      question: "Which one is correct?",
+      choices: ["Wrong", "Correct", "Wrong", "Wrong"],
+      correct: "Correct"
+    },
+    {
+      question: "It's black, it's ___.",
+      choices: ["Orange", "Magenta", "Black", "White"],
+      correct: "White"
+    },
+    {
+      question: "Ain't nobody got ___ for that!",
+      choices: ["Time", "Jello", "Money", "Life"],
+      correct: "Time"
+    },
+    {
+      question: "Wake up in the morning feeling ___.",
+      choices: ["Like Madonna", "I'm dead", "Like P. Diddy", "Refreshed"],
+      correct: "Like P. Diddy"
+    }
+  ];
 }
 
 function startQuiz() {
   quizStart.setAttribute("class", "hide");
   quizSection.setAttribute("class", "show");
+  questionsLeft = 3;
+
+  getQuestions();
   setQuiz();
 }
 
@@ -23,20 +55,27 @@ function endQuiz() {
 }
 
 function setQuiz() {
-  quizSection.getElementsByTagName("p").textContent = questionFormat.question;
+  var qIndex = Math.floor(Math.random() * quizQuestions.length);
+  qFormat = quizQuestions[qIndex];
+  quizSection.getElementsByTagName("p")[0].textContent = qFormat.question;
+  
   for (let i = 0; i < questionList.children.length; i++) {
-    questionList.children[i].textContent = questionFormat.choices[i];
+    questionList.children[i].textContent = qFormat.choices[i];
   }
+  
+  quizQuestions.splice(qIndex, 1);
 }
 
 function clicked(evt) {
-  console.log("This was clicked: " + evt.target.textContent);
-  showFeedback(evt.target.textContent)
-  endQuiz();
+  questionsLeft--;
+  if (questionsLeft === 0) {
+    endQuiz();
+  } else {
+    setQuiz();
+  }
 }
 
 function restartQuiz(evt) {
-  console.log("This was clicked: " + evt.target.textContent);
   quizEnd.setAttribute("class", "hide");
   quizStart.setAttribute("class", "show");
 }
